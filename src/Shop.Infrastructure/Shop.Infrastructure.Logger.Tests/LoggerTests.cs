ced5b51577ef96ff24f80313625e5682d1a01b24
Eugene <jekpot@ukr.net>
2013-04-11 23:43:24 +0300

    Database test

--- src/Shop.Infrastructure/Shop.Infrastructure.Logger.Tests/LoggerTests.cs
+++ src/Shop.Infrastructure/Shop.Infrastructure.Logger.Tests/LoggerTests.cs
@@ -13,19 +13,18 @@ namespace Shop.Infrastructure.Logger.Tests
     public class LoggerTests
     {
         string path = "";
+        string infoLog = "InfoTEST";
+        string warningLog = "WarningTEST";
+        string errorLog = "ErrorTEST";
+        string fatalLog = "FatalTEST";
+        string debugLog = "DebugTest";
+        string exceptionLog = new InvalidCastException().Message;
 
-        [TestMethod]
-        public void WriteLogTest()
+        [TestInitialize]
+        public void InitTests()
         {
             var logger = Factory.Factory.GetComponent<ILogger>();
             log4net.Config.XmlConfigurator.Configure();
-
-            var infoLog = "InfoTEST";
-            var warningLog = "WarningTEST";
-            var errorLog = "ErrorTEST";
-            var fatalLog = "FatalTEST";
-            var debugLog = "DebugTest";
-            var exceptionLog = new InvalidCastException().Message;
 
             logger.InfoLog(infoLog);
             logger.WarningLog(warningLog);
@@ -33,7 +32,11 @@ namespace Shop.Infrastructure.Logger.Tests
             logger.FatalLog(fatalLog);
             logger.DebugLog(debugLog);
             logger.ExceptionLog(new InvalidCastException());
+        }
 
+        [TestMethod]
+        public void WriteLogTest()
+        {
             path = AppDomain.CurrentDomain.BaseDirectory + "/Log.log";
             var logString = "";
 
@@ -55,16 +58,47 @@ namespace Shop.Infrastructure.Logger.Tests
                     {
                         logString += line;
                     }
-                    
+                }
+            }
+            Assert.IsTrue(logString.Contains(infoLog));
+            Assert.IsTrue(logString.Contains(warningLog));
+            Assert.IsTrue(logString.Contains(errorLog));
+            Assert.IsTrue(logString.Contains(fatalLog));
+            Assert.IsTrue(logString.Contains(debugLog));
+            Assert.IsTrue(logString.Contains(exceptionLog));
+        }
+
+        [TestMethod]
+        public void DatabaseLogTest()
+        {
+            string conString = "Server=(local); database=ShopTest; Trusted_Connection=Yes;Integrated Security=true";
+
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                string logMessages = "";
+                string sql = "SELECT [Message] FROM [ShopTest].[dbo].[Logs]";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    logMessages += reader[0];
                 }
 
-                Assert.IsTrue(logString.Contains(infoLog));
-                Assert.IsTrue(logString.Contains(warningLog));
-                Assert.IsTrue(logString.Contains(errorLog));
-                Assert.IsTrue(logString.Contains(fatalLog));
-                Assert.IsTrue(logString.Contains(debugLog));
-                Assert.IsTrue(logString.Contains(exceptionLog));               
-            }                     
+                reader.Close();
+
+                Assert.IsTrue(logMessages.Contains(infoLog));
+                Assert.IsTrue(logMessages.Contains(warningLog));
+                Assert.IsTrue(logMessages.Contains(errorLog));
+                Assert.IsTrue(logMessages.Contains(fatalLog));
+                Assert.IsTrue(logMessages.Contains(debugLog));
+                Assert.IsTrue(logMessages.Contains(exceptionLog));
+
+                sql = @"DELETE FROM [ShopTest].[dbo].[Logs];";
+                cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();               
+            }
         }
 
         [TestCleanup]
